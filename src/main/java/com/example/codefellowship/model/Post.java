a427f9b416aefd5a3c02fe59b3d65066339d1ee7
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-10-18 18:47:19 +0300

    fin lab-17

---
+++ src/main/java/com/example/codefellowship/model/Post.java
@@ -0,0 +1,67 @@
+package com.example.codefellowship.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    private String body;
+
+    @CreationTimestamp
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "appUser_id")
+    private ApplicationUser applicationUser;
+
+    public Post() {
+    }
+
+    public Post(String body, ApplicationUser applicationUser) {
+        this.body = body;
+        this.applicationUser = applicationUser;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ApplicationUser getApplicationUser() {
+        return applicationUser;
+    }
+
+    public void setApplicationUser(ApplicationUser applicationUser) {
+        this.applicationUser = applicationUser;
+    }
+}
